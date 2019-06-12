c285475222e1833047032b68e99ed2582f99f494
TulsiJain <tulsi.jain@delphix.com>
2019-06-12 15:57:07 -0700

    project2 setup

--- module/zfs/spa_errlog.c
+++ module/zfs/spa_errlog.c
@@ -139,7 +139,7 @@ spa_get_errlog_size(spa_t *spa)
 	uint64_t total = 0, count;
 
 	#if _KERNEL
-		printf("total is %llu\n", total);
+		printk("total is %llu\n", total);
 	#endif
 
 	mutex_enter(&spa->spa_errlog_lock);
@@ -149,7 +149,7 @@ spa_get_errlog_size(spa_t *spa)
 		total += count;
 
 	#if _KERNEL
-		printf("total is %llu\n", total);
+		printk("total is %llu\n", total);
 	#endif
 
 	if (spa->spa_errlog_last != 0 && !spa->spa_scrub_finished &&
@@ -158,18 +158,18 @@ spa_get_errlog_size(spa_t *spa)
 		total += count;
 
 	#if _KERNEL
-		printf("total is %llu\n", total);
+		printfk("total is %llu\n", total);
 	#endif
 	mutex_exit(&spa->spa_errlog_lock);
 
 	mutex_enter(&spa->spa_errlist_lock);
 	total += avl_numnodes(&spa->spa_errlist_last);
 	#if _KERNEL
-		printf("total is %llu\n", total);
+		printk("total is %llu\n", total);
 	#endif
 	total += avl_numnodes(&spa->spa_errlist_scrub);
 	#if _KERNEL
-		printf("total is %llu\n", total);
+		printk("total is %llu\n", total);
 	#endif
 	mutex_exit(&spa->spa_errlist_lock);
 
