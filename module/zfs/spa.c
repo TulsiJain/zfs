006f7598f2c12e87f616460a877496e0f0b44865
TulsiJain <tulsi.jain@delphix.com>
2019-06-17 12:00:16 -0700

    phase 2 setup

--- module/zfs/spa.c
+++ module/zfs/spa.c
@@ -7312,8 +7312,8 @@ spa_scan(spa_t *spa, pool_scan_func_t func)
 		dsl_pool_t *dp = spa->spa_dsl_pool;
 		dsl_scan_t *scn = dp->dp_scan;
 		dsl_scan_phys_t scn_phys = scn->scn_phys;
-		printk("scn_to_examine, %llu\n",  scn_phys->scn_to_examine);
-		printk("scn_examined, %llu\n",  scn_phys->scn_examined);
+		printk("scn_to_examine, %llu\n",  scn_phys.scn_to_examine);
+		printk("scn_examined, %llu\n",  scn_phys.scn_examined);
 	#endif
 	
 
