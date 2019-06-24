97c99db8a255ea3461733605d7335483f6b5e210
TulsiJain <tulsi.jain@delphix.com>
2019-06-24 15:42:48 -0700

    phase 2 setup

--- module/zfs/dsl_scan.c
+++ module/zfs/dsl_scan.c
@@ -910,7 +910,7 @@ dsl_scrub_err_sync(void *arg, dmu_tx_t *tx)
 
 	// // printk("SPA opened successfully \n");
 	
-	error = spa_get_errlog(spa, (void *)zc->zc_nvlist_dst,
+	error = spa_get_errlog(spa, (void *)(uintptr_t)zc->zc_nvlist_dst,
 	    &count);
 
 	#ifdef _KERNEL
